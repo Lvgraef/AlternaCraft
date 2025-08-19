f01e8e2ddea966a934e39e3d83ba73ea435a0d97
HuskyTacodile <huskytakidile@gmail.com>
2025-08-19 16:42:13 -0500

    some renaming and organizing

--- src/main/java/io/github/itskillerluc/init/ConsumableProperties.java
+++ src/main/java/io/github/itskillerluc/init/ConsumableProperties.java
@@ -6,10 +6,12 @@ import net.minecraft.world.food.FoodProperties;
 
 public class ConsumableProperties {
     public static final FoodProperties ALTERNABERRY = new FoodProperties.Builder().nutrition(3).saturationModifier(0.3f)
-            .effect(() -> new MobEffectInstance(MobEffects.INVISIBILITY, 80),0.5f).fast().build();
+            .effect(() -> new MobEffectInstance(MobEffects.INVISIBILITY, 80),1f).fast().build();
 
     public static final FoodProperties SHOCKBERRY = new FoodProperties.Builder().nutrition(3).saturationModifier(0.3f)
-            .effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 600),1f).fast().build();
+            .effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400),1f).fast().build();
+
+    public static final FoodProperties CHERRY = new FoodProperties.Builder().nutrition(4).saturationModifier(0.3f).fast().build();
 
     public static final FoodProperties RAW_DINO = new FoodProperties.Builder().nutrition(4).saturationModifier(0.5f).build();
 
